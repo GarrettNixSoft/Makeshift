8e94a19ae2417d7e58e4bff688c1e7e98a3ebb2d
Garrett Nix <51683504+GarrettNixSoft@users.noreply.github.com>
2023-07-13 14:34:58 -0700

    Content Browser now has columns with buttons with icons

--- Makeshift-Workshop/src/Panels/ContentBrowserPanel.cpp
+++ Makeshift-Workshop/src/Panels/ContentBrowserPanel.cpp
@@ -10,7 +10,8 @@ namespace Makeshift {
 
 	ContentBrowserPanel::ContentBrowserPanel()
 		: m_CurrentDirectory(s_AssetPath) {
-		
+		m_DirectoryIcon = Texture2D::Create("resources/icons/content_browser/directory_icon.png");
+		m_FileIcon = Texture2D::Create("resources/icons/content_browser/file_icon.png");
 	}
 
 	void ContentBrowserPanel::onImGuiRender() {
@@ -24,6 +25,16 @@ namespace Makeshift {
 			}
 		}
 
+		static float padding = 16.0f;
+		static float thumbnailSize = 128;
+		float cellSize = thumbnailSize + padding;
+
+		float panelWidth = ImGui::GetContentRegionAvail().x;
+		int columnCount = (int) (panelWidth / cellSize);
+		columnCount = std::max(columnCount, 1);
+
+		ImGui::Columns(columnCount, 0, false);
+
 		// Iterate through assets directory
 		for (auto& directoryEntry : std::filesystem::directory_iterator(m_CurrentDirectory)) {
 
@@ -33,23 +44,32 @@ namespace Makeshift {
 			std::string relativePathString = relativePath.u8string();
 			std::string filenameString = relativePath.filename().string();
 
-			// DOWN DIR BUTTONS
+			// DIR BUTTONS
 			if (directoryEntry.is_directory()) {
-				if (ImGui::Button(filenameString.c_str())) {
-					
+				ImGui::ImageButton((ImTextureID)m_DirectoryIcon->getRendererId(), { thumbnailSize, thumbnailSize }, { 0, 1 }, { 1, 0 });
+				if (ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left)) {
 					m_CurrentDirectory /= path.filename();
-
 				}
 			}
+			// FILE BUTTONS
 			else {
-				if (ImGui::Button(filenameString.c_str())) {
-
+				ImGui::ImageButton((ImTextureID)m_FileIcon->getRendererId(), { thumbnailSize, thumbnailSize }, { 0, 1 }, { 1, 0 });
+				if (ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left)) {
+					// TODO: Open asset file
 				}
 			}
-			
+			ImGui::TextWrapped(filenameString.c_str());
+
+			ImGui::NextColumn();
 
 		}
 
+		ImGui::Columns(1);
+
+		ImGui::SliderFloat("Thumbnail Size", &thumbnailSize, 64, 256);
+		ImGui::SliderFloat("Padding", &padding, 0, 32);
+
+		// TODO: status bar
 		ImGui::End();
 
 	}
--- Makeshift-Workshop/src/Panels/ContentBrowserPanel.hpp
+++ Makeshift-Workshop/src/Panels/ContentBrowserPanel.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Makeshift/Renderer/Texture.hpp"
+
 #include <imgui/imgui.h>
 
 #include <filesystem>
@@ -14,6 +16,9 @@ namespace Makeshift {
 
 	private:
 		std::filesystem::path m_CurrentDirectory;
+
+		Ref<Texture2D> m_DirectoryIcon;
+		Ref<Texture2D> m_FileIcon;
 	};
 
 }
